64526fa1d9943803a0f242843bfd5bfdfd005344
jnahan <jenna5376@gmail.com>
2025-03-01 13:18:16 -0500

    updated model path

--- src/view/DesignView.ts
+++ src/view/DesignView.ts
@@ -16,23 +16,22 @@ import {
 	PlaneBufferGeometry,
 	DoubleSide,
 	Clock,
-	Color
+	Color,
 } from 'three';
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { BaseView3D } from "./BaseView3D";
+import { BaseView3D } from './BaseView3D';
 
-const aspect = window.innerWidth / window.innerHeight
+const aspect = window.innerWidth / window.innerHeight;
 
-export class DesignView extends BaseView3D{
-
+export class DesignView extends BaseView3D {
 	group: Group;
 	exampleModel: Group;
 	exampleTexture: Texture;
 	lightAmbient: AmbientLight;
 	lightPoint: PointLight;
 
-	constructor(model: any, renderer: WebGLRenderer){
+	constructor(model: any, renderer: WebGLRenderer) {
 		super(model, renderer);
 
 		this.exampleModel = new Group();
@@ -51,19 +50,18 @@ export class DesignView extends BaseView3D{
 		this.lightPoint.castShadow = true;
 		this.lightPoint.intensity = 0.4;
 		this.scene.add(this.lightPoint);
-		
-		const modelLoader = new GLTFLoader().setPath('../resources/models/');
+
+		const modelLoader = new GLTFLoader().setPath('/room-builder/resources/models/');
 		modelLoader.load('room.gltf', (gltf) => {
-			
 			this.exampleModel = gltf.scene;
-			this.exampleModel.position.set(0,-0.5,0);
+			this.exampleModel.position.set(0, -0.5, 0);
 			this.exampleModel.scale.set(1, 1, 1);
 
-			const newMat = new MeshPhongMaterial({color : 0xabdbb8})
-			
+			const newMat = new MeshPhongMaterial({ color: 0xabdbb8 });
+
 			this.exampleModel.traverse((child: THREE.Object3D<THREE.Event>) => {
-				if (child.type == 'mesh'){
-						(child as gltfMesh).material = newMat;
+				if (child.type == 'mesh') {
+					(child as gltfMesh).material = newMat;
 				}
 			});
 			this.group.add(this.exampleModel);
